81a93b48db7b623c659738a78bbf6eb66b40a5b7
Napat <napat.wongchr@gmail.com>
2022-03-14 22:28:43 +0700

    add ex

---
+++ ex-1.js
@@ -0,0 +1,14 @@
+function addByX(x) {
+  // Start coding here
+}
+
+// Uncomment โค้ดด้านล่างเพื่อดูผลลัพธ์ของการทำงาน
+
+// const addByTwo = addByX(2);
+// addByTwo(1); // => should return 3
+// addByTwo(2); // => should return 4
+// addByTwo(3); // => should return 5
+
+// const addByThree = addByX(3);
+// addByThree(1); // => should return 4
+// addByThree(2); // => should return 5
