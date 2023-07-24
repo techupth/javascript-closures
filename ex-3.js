e6fb1c94157b67456a2c1f519277f8827cd0604c
Mind Sathirasrindh <109655871+mydjolie@users.noreply.github.com>
2023-07-25 00:17:39 +0700

    feat:add lp and github sync and add ex3-4

--- ex-3.js
+++ ex-3.js
@@ -1,16 +1,33 @@
-//Exercise#3 Debugging
+//Exercise#3 : Debugging 1
 
 //Start coding here
 
-function removeDuplicates(array) {
-  let uniqueArray = [];
-  for (let i = 0; i < array.length - 1; i++) {
-    if (uniqueArray.indexOf(array[i]) === 1) {
-      uniqueArray.push(array[0]);
-    }
+let productSales = [
+  { id: 1, menu: "ลาเต้เย็น", amount: 120, sales: 1200000.22 },
+  { id: 2, menu: "คาปูชิโน่", amount: 1, sales: 32000 },
+  { id: 3, menu: "ข้าวผัดอเมริกัน", amount: 20, sales: 1000 },
+  { id: 4, menu: "แพนเค้กช็อกโกแลต", amount: 50, sales: 2000 },
+  { id: 5, menu: "ไอศกรีมชาไทย", amount: 50, sales: 222200 },
+  { id: 6, menu: "ลาเต้เย็น", amount: 120, sales: 12000 },
+  { id: 7, menu: "คาปูชิโน่", amount: 1, sales: 32000 },
+  { id: 8, menu: "ข้าวผัดอเมริกัน", amount: 20, sales: 1000 },
+  { id: 9, menu: "แพนเค้กช็อกโกแลต", amount: 50, sales: 2000 },
+  { id: 10, menu: "ไอศกรีมชาไทย", amount: 50, sales: 222200 },
+  { id: 11, menu: "ลาเต้เย็น", amount: 120, sales: 12000 },
+  { id: 12, menu: "คาปูชิโน่", amount: 1, sales: 32000 },
+  { id: 13, menu: "ข้าวผัดอเมริกัน", amount: 20, sales: 1000 },
+  { id: 14, menu: "แพนเค้กช็อกโกแลต", amount: 50, sales: 2000 },
+  { id: 15, menu: "ไอศกรีมชาไทย", amount: 50, sales: 222200 },
+];
+
+function calculateTotalSales(items) {
+  let total = 0;
+  for (let item of items) {
+    total = total + item.sale;
   }
-  return uniqueArray;
+  return total;
 }
 
-let numbers = [1, 2, 2, 3, 4, 4, 5];
-console.log(removeDuplicates(numbers));
+let total = calculateTotalSale();
+
+console.log("Total sales: ", total);
